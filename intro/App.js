482a1b0c873d1b565c16e4ac4e6cf8ecde9b51bd
ViniciusSoar3s <vinicius.soare3s@gmail.com>
2025-04-02 10:29:19 -0300

    Meu primeiros componentes react-native

--- intro/App.js
+++ intro/App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Meu primeiro aplicativo 1</Text>
+      <Image source={require('./assets/bomba.jpg')} style={styles.logo}/>
+      <Text style={styles.text2}>Meu primeiro aplicativo 2</Text>
+      <Image source={require('./assets/explosão.jpg')} style={styles.logo2}/>
+      <TextInput style={styles.input} placeholder='Digite seu nome'/>
+      <Button title='Salvar'/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +22,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    frontsize: 15,
+    fontWeight: 'bold',
+    color: 'green',
+  },
+  logo: {
+    width: 50,
+    height: 100,
+    marginBotton: 10,
+    opacity: 1,
+  },
+  text2: {
+    frontsize: 10,
+    fontWeight: 'bold',
+    color: 'purple',
+  },
+  logo2: {
+    width: 50,
+    height: 50,
+    marginBotton: 10,
+    opacity: 1,
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBotton:20,
+  }
 });
